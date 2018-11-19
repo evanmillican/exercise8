a7b86eab3c0bb6c144ea028471f7146956dfd33e
evanmillican <evanmill@live.unc.edu>
2018-11-19 10:09:43 -0500

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 $(function(){
   console.log('scripts loaded');
 
+var lat = '';
+var long = '';
 
 var url = 'http://api.open-notify.org/iss-now.json';
-/*var url2 = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + issData.iss_position.latitude + '&lon=' + issData.iss_position.longitude;
-var urlArray = [url, url2];*/
+var url2 = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-36.0515&lon=-101.776';
+
 var issData = [];
+var geoData = [];
 var html = '';
 var iss_position = [];
-var i;
+var address = [];
+var place_id = [];
+
+
+
+
 
 
 
@@ -28,10 +36,33 @@ $.ajax({
   data:issData,
   success:function(issData){
     console.log(issData.iss_position);
+    lat = issData.iss_position.latitude;
+    long = issData.iss_position.longitude;
 
-    html += 'The current location of the international space station is: ' + issData.iss_position.latitude + ', ' + issData.iss_position.longitude +'<br>';
 
-    $('#results').html(html);
+    $.ajax({
+      type:'GET',
+      url: url2,
+      dataType:'json',
+      async:true,
+      data:geoData,
+      success:function(geoData){
+        console.log(geoData);
+
+
+        html += 'The current location of the international space station is: ' + geoData.address +'<br>';
+
+        if(geoData.address = 'error') {
+          html += 'The current location of the international space station is over an ocean!<br>';
+        }
+
+        $('#results').html(html);
+
+      }
+
+    });
+
+
 
 
   }
